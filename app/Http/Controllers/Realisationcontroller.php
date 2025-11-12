0e1b6161c6706db69332f3f592038ccda7ead63a
Lipton <rachelleberthier2@gmail.com>
2025-11-12 09:19:22 +0100

    correction du RealisationController

--- app/Http/Controllers/Realisationcontroller.php
+++ app/Http/Controllers/Realisationcontroller.php
@@ -8,14 +8,21 @@ class RealisationController extends Controller
 {
    public function index(Request $request)
 {
-    $category = $request->query('category');
-
     // Liste fixe des catégories
     $categories = ['Créa visuel', 'Projet design', 'Montage vidéo', 'Campagne médias'];
-
-    $realisations = Realisation::when($category, function ($query, $category) {
-        return $query->where('category', $category);
-    })->latest()->get();
+    
+    
+    
+    // $category = $request->query('category'); si category existe
+    if($request->has('category')){
+        $category = $request->query('category');
+        $realisations = Realisation::when($category, function ($query, $category) {
+            return $query->where('category', $category);
+        })->latest()->get();
+    } else {
+        $category = null;
+        $realisations = Realisation::latest()->get()->pagi;
+    }
 
     return view('realisations', compact('realisations', 'categories', 'category'));
 }
