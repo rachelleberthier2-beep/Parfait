65a0e64c6dc7f2ab26500adaefb6eb7fffcb5b92
Lipton <rachelleberthier2@gmail.com>
2025-11-13 05:23:24 +0100

    feat(views): ajout des liens dans la vue dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,6 +6,13 @@
 <body>
     <h1>Bienvenue, {{ auth()->user()->name }}</h1>
 
+     <nav>
+        <ul>
+            <li><a href="{{ url('/blog/create') }}">Ajouter un article</a></li>
+            <li><a href="{{ url('/realisation/create') }}">Ajouter une réalisation</a></li>
+        </ul>
+    </nav>
+
     <form method="POST" action="{{ route('logout') }}">
         @csrf
         <button type="submit">Se déconnecter</button>
