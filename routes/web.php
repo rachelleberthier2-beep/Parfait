9d0501581e0f0fbb8cd920893a4ec9d83f97ee03
Lipton <rachelleberthier2@gmail.com>
2025-11-18 08:06:53 +0100

    feat(web):ajout de nouvelle routes dans web

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,8 @@ use App\Http\Controllers\{
     ContactController
 };
 
+Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register.form');
+Route::post('/register', [AuthController::class, 'register'])->name('register');
 
 Route::get('login', [AuthController::class, 'showLoginForm'])->name('login.form');
 Route::post('login', [AuthController::class, 'login'])->name('login');
