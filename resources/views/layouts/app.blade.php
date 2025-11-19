78087d3105e4b74bbe41765661fd7a0c8c0ea1a2
Lipton <rachelleberthier2@gmail.com>
2025-11-19 11:04:53 +0100

    mise a jour

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=1, user-scalable=no">
-    <title>{{ config('app.name') }} - @yield('title')</title>
+    <title>@yield('title', 'Parfaitcommunicationg')</title>
     @vite(['resources/css/app.css', 'resources/css/panneau.css', 'resources/js/app.js'])
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <link href="https://unpkg.com/aos@2.3.4/dist/aos.css" rel="stylesheet">
